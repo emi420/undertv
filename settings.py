645854ab04c3755847da661f5f3d6f830ff2d84d
Emilio Mariscal <emi420@gmail.com>
2016-05-16 18:57:02 -0300

    readme

--- settings.py
+++ settings.py
@@ -1,5 +1,4 @@
 settings = {}
-settings['BASE_PATH'] = '/home/pi/tv/'
 settings['VIDEO_WAITING_PATH'] = '/home/pi/tv/bars.mp4'
 settings['CONTINUOUS_PLAYBACK'] = True
 settings['VIDEO_PATH'] = '/home/pi/tv/'
