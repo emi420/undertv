ee5827edf5647da4eb0a6ef872ac0e78b1eae80d
Emilio Mariscal <emi420@gmail.com>
2016-05-16 18:51:00 -0300

    settigs

--- settings.py
+++ settings.py
@@ -1,5 +1,6 @@
 settings = {}
 settings['BASE_PATH'] = '/home/pi/tv/'
-settings['VIDEO_PATH'] = '/mnt/usbstorage/'
 settings['VIDEO_WAITING_PATH'] = '/home/pi/tv/bars.mp4'
 settings['CONTINUOUS_PLAYBACK'] = True
+settings['VIDEO_PATH'] = '/home/pi/tv/'
+#settings['VIDEO_PATH'] = '/mnt/usbstorage/'
